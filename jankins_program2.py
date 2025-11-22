fb30fd80f15c29262ff8b07bd5307b13f70e1f73
gouri <smchikkeri5757@gmail.com>
2025-11-22 10:08:40 +0530

    Update jankins_program2.py

--- jankins_program2.py
+++ jankins_program2.py
@@ -1,7 +1,7 @@
 import sys
 if len(sys.argv) !=3:
-  print("usage: python student.py <name><rollno>")
-  sys.exit(1)
+    print("usage: python student.py <name><rollno>")
+    sys.exit(1)
   script_name = sys.argv[0]
   name = sys.argv[1]
   rollno = sys.argv[2]
